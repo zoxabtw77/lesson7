6449af5038e021901a8813d3887fdbdf074cccb2
Zoxir Rustamov <zoxabtw77@gmail.com>
2021-11-20 18:09:11 +0500

    lesson 7

---
+++ index.py
@@ -0,0 +1,23 @@
+# names = ['Zokhir', 'Shoxa', 'Zikir']
+# ages = [18, 18, 19]
+
+# names.append('Ali')
+# ages.append(17)
+#УДАЛИТЕЛЬ ЭЛЕМЕНТОВ 
+# names.pop()
+#добавление по значению
+# names.insert(0, 'Shaxa')
+# ages.insert(0, 17)
+
+#print(len(names))
+
+#Удаление по значению так и с цыфрами 
+# names.remove('Zikir')
+
+#переварачивает исходный массив
+# names.reverse()
+#(::-1) ТО СПРАВА НА ЛЕВО 
+
+
+# print(names) 
+
